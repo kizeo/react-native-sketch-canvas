99b8c3f3cd2f9c375c17f2d203b5541c9277f3b4
Agostinho Quintela <quintela@kizeo.com>
2019-12-10 09:51:22 +0100

    feat: tranfert project to

---
+++ src/handlePermissions.js
@@ -0,0 +1,15 @@
+import { Platform } from 'react-native'
+import * as Permissions from 'expo-permissions'
+
+export const requestPermissions = async (permissionDialogTitle, permissionDialogMessage) => {
+  if (Platform.OS === 'android') {
+    const { status } = await Permissions.askAsync(Permissions.WRITE_EXTERNAL_STORAGE)
+
+    // On devices before SDK version 23, the permissions are automatically granted if they appear in the manifest,
+    // so check and request should always be true.
+    // https://github.com/facebook/react-native-website/blob/master/docs/permissionsandroid.md
+    const isAuthorized = status === 'granted'
+    return isAuthorized
+  }
+  return true
+}
